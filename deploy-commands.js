a9d4ef2012c6c7bf0568e5b59dfe38a8aca0eac3
alsksssass <112150208+alsksssass@users.noreply.github.com>
2022-08-26 15:05:51 +0900

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -52,7 +52,7 @@ const commands = [
 ]
   .map(command => command.toJSON());
 
-const rest = new REST({ version: '10' }).setToken(token);
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
   .then(() => console.log('코멘드 등록 성공!'))
