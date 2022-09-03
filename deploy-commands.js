c8cf888e7563e36d330f43bef61ee80a2e377bdf
alsksssass <112150208+alsksssass@users.noreply.github.com>
2022-09-04 01:30:31 +0900

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -69,6 +69,8 @@ const commands = [
   new SlashCommandBuilder().setName('전화기초기화').setDescription('전화기습득초기화').setDefaultMemberPermissions(0),
   new SlashCommandBuilder().setName('패턴').setDescription('패턴 잠금 풀기').addStringOption(option => option.setName('패턴').setDescription('번호적기')),
   new SlashCommandBuilder().setName('페어폰').setDescription('패턴용 페어폰 습득').setDefaultMemberPermissions(0),
+  new SlashCommandBuilder().setName('시계발견').setDescription('시계발견').setDefaultMemberPermissions(0),
+  
 ]
   .map(command => command.toJSON());
 
