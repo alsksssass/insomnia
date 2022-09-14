9cb812947262ac13ddff9c32dc2f5758b1891f88
alsksssass <112150208+alsksssass@users.noreply.github.com>
2022-09-14 19:42:29 +0900

    Update index.js

--- index.js
+++ index.js
@@ -672,7 +672,8 @@ tpttlrks *=0
     await interaction.reply("⏳"+parseInt((timecc / 60)/60)+"시간"+parseInt((timecc / 60)%60)+"분"+(timecc % 60)+"초 타이머 시작")
     var timer = setInterval(() => {
         timecc -= 1
-        interaction.editReply({ content: "⏳"+parseInt((timecc / 60)/60)+"시간"+parseInt((timecc / 60)%60)+"분"+(timecc % 60)+"초남음" });
+        interaction.editReply({ content: "⏳"+parseInt((timecc / 60)/60)+"시간"+parseInt((timecc / 60)%60)+"분"+(timecc % 60)+"초남음" })
+      .catch(console.error);
         if (timecc === 0) {
             clearInterval(timer);
             interaction.editReply({ content: "⌛"+"시간종료!" });
